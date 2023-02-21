786426012f4481e9f288063a079fbb6920917741
spoddub <spodd98@gmail.com>
2023-02-21 20:02:28 +0300

    finished

---
+++ triple.py
@@ -0,0 +1,13 @@
+def make(element1, element2, element3):
+    return {'element1': element1, 'element2': element2, 'element3': element3}
+
+
+def get1(triple):
+    return triple['element1']
+
+
+def get2(triple):
+    return triple['element2']
+
+def get3(triple):
+    return triple['element3']
